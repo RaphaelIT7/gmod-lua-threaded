2444dae9d56d28d11b6670e4ccacef2117ed5b75
RaphaelIT7 <raphael.i.t@outlook.de>
2023-11-18 04:34:19 +0100

    If we want to use lua function we should add them!

--- source/detours.cpp
+++ source/detours.cpp
@@ -10,6 +10,16 @@ TAdvancedLuaErrorReporter func_AdvancedLuaErrorReporter;
 Tlua_atpanic func_lua_atpanic;
 luaL_loadstring func_luaL_loadstring;
 
+luaopen_base func_luaopen_base;
+luaopen_bit func_luaopen_bit;
+luaopen_debug func_luaopen_debug;
+luaopen_jit func_luaopen_jit;
+luaopen_math func_luaopen_math;
+luaopen_os func_luaopen_os;
+luaopen_package func_luaopen_package;
+luaopen_string func_luaopen_string;
+luaopen_table func_luaopen_table;
+
 InitLuaLibraries func_InitLuaLibraries;
 InitLuaClasses func_InitLuaClasses;
 
@@ -47,6 +57,39 @@ void Symbols_Init()
 	func_luaL_loadstring = (luaL_loadstring)symfinder.Resolve(lua_shared_loader.GetModule(), luaL_loadstringSym.name.c_str(), luaL_loadstringSym.length);
 	CheckFunction(func_luaL_loadstring, "luaL_loadstring");
 
+	/*
+		Lua open stuff
+	*/
+	func_luaopen_base = (luaopen_base)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_baseSym.name.c_str(), luaopen_baseSym.length);
+	CheckFunction(func_luaopen_base, "luaopen_base");
+
+	func_luaopen_bit = (luaopen_bit)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_bitSym.name.c_str(), luaopen_bitSym.length);
+	CheckFunction(func_luaopen_bit, "luaopen_bit");
+
+	func_luaopen_debug = (luaopen_debug)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_debugSym.name.c_str(), luaopen_debugSym.length);
+	CheckFunction(func_luaopen_debug, "luaopen_debug");
+
+	func_luaopen_jit = (luaopen_jit)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_jitSym.name.c_str(), luaopen_jitSym.length);
+	CheckFunction(func_luaopen_jit, "luaopen_jit");
+
+	func_luaopen_math = (luaopen_math)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_mathSym.name.c_str(), luaopen_mathSym.length);
+	CheckFunction(func_luaopen_math, "luaopen_math");
+
+	func_luaopen_os = (luaopen_os)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_osSym.name.c_str(), luaopen_osSym.length);
+	CheckFunction(func_luaopen_os, "luaopen_os");
+
+	func_luaopen_package = (luaopen_package)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_packageSym.name.c_str(), luaopen_packageSym.length);
+	CheckFunction(func_luaopen_package, "luaopen_package");
+
+	func_luaopen_string = (luaopen_string)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_stringSym.name.c_str(), luaopen_stringSym.length);
+	CheckFunction(func_luaopen_string, "luaopen_string");
+
+	func_luaopen_table = (luaopen_table)symfinder.Resolve(lua_shared_loader.GetModule(), luaopen_tableSym.name.c_str(), luaopen_tableSym.length);
+	CheckFunction(func_luaopen_table, "luaopen_table");
+
+	/*
+		Server suff
+	*/
 	SourceSDK::ModuleLoader server_loader("server");
 	func_InitLuaLibraries = (InitLuaLibraries)symfinder.Resolve(server_loader.GetModule(), InitLuaLibrariesSym.name.c_str(), InitLuaLibrariesSym.length);
 	CheckFunction(func_InitLuaLibraries, "InitLuaLibraries");
