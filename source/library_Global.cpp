82c908aa1c6842de4544eeb60ac882e79acb839f
RaphaelIT7 <raphael.i.t@outlook.de>
2023-11-27 18:19:04 +0100

    Added Vector and Angle

--- source/library_Global.cpp
+++ source/library_Global.cpp
@@ -9,10 +9,30 @@ LUA_FUNCTION(include)
 	return 0;
 }
 
+LUA_FUNCTION(LVector)
+{
+	int x = LUA->CheckNumber(1);
+	int y = LUA->CheckNumber(2);
+	int z = LUA->CheckNumber(3);
+
+	LUA->PushVector(Vector(x, y, z));
+}
+
+LUA_FUNCTION(Angle)
+{
+	int x = LUA->CheckNumber(1);
+	int y = LUA->CheckNumber(2);
+	int z = LUA->CheckNumber(3);
+
+	LUA->PushAngle(QAngle(x, y, z));
+}
+
 void InitGlobal(ILuaInterface* LUA)
 {
 	LUA->PushSpecial(SPECIAL_GLOB);
 		Add_Func(LUA, include, "include");
+		Add_Func(LUA, LVector, "Vector");
+		Add_Func(LUA, Angle, "Angle");
 
 	LUA->Pop(1);
 }
