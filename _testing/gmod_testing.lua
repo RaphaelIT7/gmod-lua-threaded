6b261689df944a7741fc40467bf56c17d7edf04b
RaphaelIT7 <raphael.i.t@outlook.de>
2024-02-24 06:06:46 +0100

    Test file.Open

--- _testing/gmod_testing.lua
+++ _testing/gmod_testing.lua
@@ -3,6 +3,10 @@ print("Testing Started")
 local code = [[
 	print(Vector(1, 1, 1), type(Vector(1, 1, 1)), isvector(Vector(1, 1, 1)))
 	print(Angle(1, 1, 1), type(Angle(1, 1, 1)), isangle(Vector(1, 1, 1)))
+
+	local gm = file.Open("garrysmod.ver", "MOD")
+	print(gm, gm:Read(gm:Size()))
+	gm:Close()
 ]]
 
 local ret, err = pcall(function()
