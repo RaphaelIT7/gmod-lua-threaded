1f84f52a0b168d907cd91548873179e2d130e1d0
RaphaelIT7 <raphael.i.t@outlook.de>
2024-02-24 05:58:23 +0100

    ToDo: Extend testing code later

--- _testing/gmod_testing.lua
+++ _testing/gmod_testing.lua
@@ -1,11 +1,15 @@
 print("Testing Started")
 
+local code = [[
+	print(Vector(1, 1, 1), type(Vector(1, 1, 1)), isvector(Vector(1, 1, 1)))
+	print(Angle(1, 1, 1), type(Angle(1, 1, 1)), isangle(Vector(1, 1, 1)))
+]]
+
 local ret, err = pcall(function()
 	require("lua_threaded")
 	iFace = LuaThreaded.CreateInterface()
 	iFace:InitGmod()
-	iFace:RunString([[print(Vector(1, 1, 1), type(Vector(1, 1, 1)), isvector(Vector(1, 1, 1)))]])
-	iFace:RunString([[print(Angle(1, 1, 1), type(Angle(1, 1, 1)), isangle(Vector(1, 1, 1)))]])
+	iFace:RunString(code)
 end)
 
 if err then
