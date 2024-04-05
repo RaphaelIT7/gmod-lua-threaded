19f3193b3faa5240bd1a1e6911acd584b6a3cf62
Equo <biobraix@outlook.fr>
2024-04-05 03:28:39 +0200

    Refactor code in library_ents.cpp

--- source/library_ents.cpp
+++ source/library_ents.cpp
@@ -3,61 +3,39 @@
 
 LUA_FUNCTION(ents_Create)
 {
-	const char* classname = LUA->CheckString(1);
-	
-	
-	//LUA->PushMetaTable(1);
+    const char* classname = LUA->CheckString(1);
+    
+    //LUA->PushMetaTable(1);
 
-	return  0; // 1;
+    return  0; // 1;
 }
 
 LUA_FUNCTION(ents_FindEntityByName)
 {
-	if (LUA->CheckString(1)) {
-		const char* classname = LUA->GetString(1);
-		// Use the FindEntityByName function
-		CBaseEntity* entity = gEntList.FindEntityByName(NULL, classname);
-		if (entity != NULL) {
-			// Entity found, do something with it...
-			printf("Entity found: %s\n", entity->GetClassname());
-		} else {
-			// Entity not found
-		}
-	} else {
-		LUA->ThrowError("Invalid classname");
-	}
-	
-	return  0;
+    if (LUA->CheckString(1)) {
+        const char* classname = LUA->GetString(1);
+        // Use the FindEntityByName function
+        CBaseEntity* entity = gEntList.FindEntityByName(NULL, classname);
+        if (entity != NULL) {
+            // Entity found, do something with it...
+            printf("Entity found: %s\n", entity->GetClassname());
+        } else {
+            // Entity not found
+        }
+    } else {
+        LUA->ThrowError("Invalid classname");
+    }
+    
+    return  0;
 }
 
-/* void FileLibThink(ILuaThread* thread)
+void InitEntsLib(GarrysMod::Lua::ILuaInterface* LUA)
 {
-	std::vector<IAsyncFile*> files;
-	ILuaInterface* LUA = thread->IFace;
-	for(IAsyncFile* file : thread->async) {
-		if (!file->finished) { continue; }
-		LUA->ReferencePush(file->callback);
-		LUA->PushString(file->req->pszFilename);
-		LUA->PushString(file->req->pszPathID);
-		LUA->PushNumber(file->Status);
-		LUA->PushString(static_cast<char*>(file->req->pData));
-		LUA->PCall(4, 0, 0);
-		LUA->ReferenceFree(file->callback);
-		files.push_back(file);
-	}
-
-	for(IAsyncFile* file : files) {
-		thread->async.erase(find(thread->async.begin(),thread->async.end(), file)); // This is probably shit.
-	}
-} */
-
-void InitEntsLib(ILuaInterface* LUA)
-{
-	LUA->PushSpecial(SPECIAL_GLOB);
-		LUA->CreateTable();
-			Add_Func(LUA, ents_Create, "Create");
-			Add_Func(LUA, ents_FindEntityByName, "FindByName");
-			
-		LUA->SetField(-2, "ents");
-	LUA->Pop();
+    LUA->PushSpecial(SPECIAL_GLOB);
+        LUA->CreateTable();
+            Add_Func(LUA, ents_Create, "Create");
+            Add_Func(LUA, ents_FindEntityByName, "FindByName");
+            
+        LUA->SetField(-2, "ents");
+    LUA->Pop();
 }
