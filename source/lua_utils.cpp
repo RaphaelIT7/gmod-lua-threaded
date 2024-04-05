b4c8efae6d6d56f3c43a894e4cbff32871771b4f
RaphaelIT7 <raphael.i.t@outlook.de>
2024-04-06 00:25:31 +0200

    Trying to implement game.KickID

--- source/lua_utils.cpp
+++ source/lua_utils.cpp
@@ -20,6 +20,7 @@ GarrysMod::Lua::ILuaInterface* Win_CreateInterface() {
 IFileSystem* filesystem;
 CGlobalVars* gpGlobal;
 IVEngineServer* engine;
+IServer* iServer;
 
 int interfaces_count = 0;
 std::unordered_map<double, ILuaThread*> interfaces;
