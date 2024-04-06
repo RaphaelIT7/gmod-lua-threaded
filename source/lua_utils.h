9acaa5253d1f85d8456def9a8fb71c60d7e2b1a9
Equo <biobraix@outlook.fr>
2024-04-06 23:30:42 +0200

    Update include statements in lua_utils.h

--- source/lua_utils.h
+++ source/lua_utils.h
@@ -259,4 +259,4 @@ extern std::string ToPath(std::string);
 extern IFileSystem* filesystem;
 extern CGlobalVars* gpGlobal;
 extern IVEngineServer* engine;
-extern IEntityInfoManager entityInfoManager;
+extern IEntityInfoManager* entityInfoManager;
