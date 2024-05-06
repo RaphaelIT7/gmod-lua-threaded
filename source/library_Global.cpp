07e51fbcaada859e4ab2c8c4eb195c9d3b4f41c4
RaphaelIT7 <raphael.i.t@outlook.de>
2024-05-06 23:31:18 +0200

    Trying to improve Msg

--- source/library_Global.cpp
+++ source/library_Global.cpp
@@ -287,43 +287,18 @@ LUA_FUNCTION(Global_Msg)
 		{
 			ss << arg_str;
 		} else {
-			int type = LUA->GetType(i);
-			bool meta = false;
-			void* ref = nullptr;
-			switch(type)
-			{
-				case GarrysMod::Lua::Type::Bool:
-					ss << (LUA->GetBool(i) ? "true" : "false");
-					break;
-				case GarrysMod::Lua::Type::Function:
-					ss << "function: 0x" << std::hex << LUA->GetUserdata(i);
-					break;
-				default:
+			LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+				LUA->GetField(1, "tostring");
+				if (LUA->IsType(-1, GarrysMod::Lua::Type::String))
+				{
 					LUA->Push(i);
-					if (LUA->GetMetaTable(-1))
-					{
-						LUA->GetField(-1, "__tostring");
-						if (LUA->IsType(-1, GarrysMod::Lua::Type::Function))
-						{
-							LUA->Push(-3);
-							GarrysMod::Lua::ILuaInterface* LLUA = (GarrysMod::Lua::ILuaInterface*)LUA;
-							LLUA->CallFunctionProtected(1, 1, true);
-							
-							if (LUA->IsType(-1, GarrysMod::Lua::Type::String))
-							{
-								meta = true;
-								ss << LUA->GetString(-1);
-							}
-						}
-						LUA->Pop(2);
-					}
+					LUA->Call(1, 1);
+					ss << LUA->GetString(-1);
 					LUA->Pop(1);
-
-					if (!meta)
-						ss << "<Something Unknown. Scary>";
-
-					break;
-			}
+				} else {
+					Msg("[LuaThreaded] Please don't nuke tostring. It's needed by Msg for now\n");
+				}
+			LUA->Pop(2);
 		}
 	}
 
